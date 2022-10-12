397b1d94f3d38df578814c42085a11c6684252ba
EMaksy <emaksymenko@suse.com>
2022-10-12 17:56:54 +0200

    Add test for coresponding link

--- assets/js/components/HealthSummary/HealthSummary.test.jsx
+++ assets/js/components/HealthSummary/HealthSummary.test.jsx
@@ -4,23 +4,18 @@ import '@testing-library/jest-dom';
 import HomeHealthSummary from './HomeHealthSummary';
 import { withState } from '@lib/test-utils';
 
-describe('HomeHealthSummary icons'),
-  () => {
-    it(
-      'should have an clickacble Pacemaker Cluster icon which links to the belonging cluster overview'
-    ),
+describe('HomeHealthSummary icons',() => {
+    it('should have an clickacble Pacemaker Cluster icon which links to the belonging cluster overview',
       () => {
-        // load data from the state
+        // load component
         const [statefulHomeHealthSummary, store] = withState(
-          <HomeHealthSummary />
-        );
-        console.log(statefulHomeHealthSummary, store);
-
+          <HomeHealthSummary />);
+          
+        store.dispatch((healthSummaryTableConfig()));
+        store.dispatch((GlobalHealth()));
+        store.dispatch((HomeHealthSummary()));
         render(statefulHomeHealthSummary);
-        // grab cluster id from
-
-        // simulate user clicking the icon?
-
-        // assert/expect the response route is like the expected
-      };
-  };
+        expect(screen.getByRole('a href')).toHaveTextContent('/routes/');
+      });
+    });
+    
