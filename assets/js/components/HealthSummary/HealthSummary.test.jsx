528345ad56810e832b1ac6750b59b2aeb3edc9a1
EMaksy <eugen.maksymenko@suse.com>
2022-10-13 14:07:14 +0200

    Add test for database icon and SAP systems

--- assets/js/components/HealthSummary/HealthSummary.test.jsx
+++ assets/js/components/HealthSummary/HealthSummary.test.jsx
@@ -1,21 +1,82 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+
+import { render, screen } from '@testing-library/react';
+import 'intersection-observer';
 import '@testing-library/jest-dom';
-import HomeHealthSummary from './HomeHealthSummary';
 import { withState } from '@lib/test-utils';
 
+import HomeHealthSummary from '.';
+import ComponentHealthSummary from  '.';
+import HealthSummary from '.';
+
 describe('HomeHealthSummary icons',() => {
-    it('should have an clickacble Pacemaker Cluster icon which links to the belonging cluster overview',
+    it('should have an clickable Pacemaker Cluster icon which links to the belonging cluster overview',
       () => {
-        // load component
+        //need an example id to check if the link is correct
+        const pacemakerId="HERE_IS_THE_TEST_ID"; // where should i grab the data.
+
         const [statefulHomeHealthSummary, store] = withState(
-          <HomeHealthSummary />);
+          <HomeHealthSummary/>);
           
-        store.dispatch((healthSummaryTableConfig()));
-        store.dispatch((GlobalHealth()));
-        store.dispatch((HomeHealthSummary()));
+        //store.dispatch((healthSummaryTableConfig()));
+        //store.dispatch((GlobalHealth()));
+        //store.dispatch(HealthSummary());
+       
+        //store.dispatch(healthSummaryTableConfig());
+
+        store.dispatch(HealthSummary());
         render(statefulHomeHealthSummary);
-        expect(screen.getByRole('a href')).toHaveTextContent('/routes/');
+
+        //console.log(render(statefulHomeHealthSummary));
+
+        //expect(screen.getByRole('href')).toHaveTextContent(`/routes/${pacemakerId}`);
+        expect(screen.getByText(`/routes/${pacemakerId}`)).toBeTruthy();
       });
+
+      it('should have an clickable Database icon which links to the belonging Database overview',
+      () => {
+        //need an example id to check if the link is correct
+        const databaseID="HERE_IS_THE_TEST_ID"; // where should i grab the data.
+
+        const [statefulHomeHealthSummary, store] = withState(
+          <HealthSummary/>);
+          
+        //store.dispatch((healthSummaryTableConfig()));
+        //store.dispatch((GlobalHealth()));
+        //store.dispatch(HealthSummary());
+       
+        //store.dispatch(healthSummaryTableConfig());
+
+        store.dispatch(healthSummaryTableConfig());
+        render(statefulHomeHealthSummary);
+
+        //console.log(render(statefulHomeHealthSummary));
+
+        expect(screen.getByText(`/databases/${databaseID}`)).toBeTruthy();
+      });
+
+      it('should have an clickable Sap Instance icon which links to the belonging SAP SYSTEMS overview',
+      () => {
+        //need an example id to check if the link is correct
+        const sapInstanceId = "HERE_IS_THE_TEST_ID"; // where should i grab the data.
+
+        const [statefulHomeHealthSummary, store] = withState(
+          <HealthSummary/>);
+          
+        //store.dispatch((healthSummaryTableConfig()));
+        //store.dispatch((GlobalHealth()));
+        //store.dispatch(HealthSummary());
+       
+        //store.dispatch(healthSummaryTableConfig());
+
+        store.dispatch(healthSummaryTableConfig());
+        render(statefulHomeHealthSummary);
+
+        //console.log(render(statefulHomeHealthSummary));
+        expect(screen.getByText(`/sap_systems/${sapInstanceId}`)).toBeTruthy();
+      });
+
+
+      
     });
     
