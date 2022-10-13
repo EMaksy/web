5576cc600ae787945c5a4414a01fd014ed322bde
EMaksy <eugen.maksymenko@suse.com>
2022-10-13 15:40:51 +0200

    Refactor the test

--- assets/js/components/HealthSummary/HealthSummary.test.jsx
+++ assets/js/components/HealthSummary/HealthSummary.test.jsx
@@ -5,75 +5,29 @@ import 'intersection-observer';
 import '@testing-library/jest-dom';
 import { withState } from '@lib/test-utils';
 
-import HomeHealthSummary from '.';
-import ComponentHealthSummary from  '.';
-import HealthSummary from '.';
+import { HomeHealthSummary } from './HomeHealthSummary';
+
+import { setHealthSummary, stopHealthSummaryLoading } from '@state/healthSummary';
 
 describe('HomeHealthSummary icons',() => {
     it('should have an clickable Pacemaker Cluster icon which links to the belonging cluster overview',
       () => {
         //need an example id to check if the link is correct
         const pacemakerId="HERE_IS_THE_TEST_ID"; // where should i grab the data.
+        const [statefulHomeHealthSummary, store] = withState(<HomeHealthSummary/>);
 
-        const [statefulHomeHealthSummary, store] = withState(
-          <HomeHealthSummary/>);
+
+        store.dispatch(setHealthSummary([{
+          id: 'blabla',
+          sid: 'blabla',
           
-        //store.dispatch((healthSummaryTableConfig()));
-        //store.dispatch((GlobalHealth()));
-        //store.dispatch(HealthSummary());
-       
-        //store.dispatch(healthSummaryTableConfig());
-
-        store.dispatch(HealthSummary());
+        }]));
+        store.dispatch(stopHealthSummaryLoading());
+        
         render(statefulHomeHealthSummary);
-
-        //console.log(render(statefulHomeHealthSummary));
 
         //expect(screen.getByRole('href')).toHaveTextContent(`/routes/${pacemakerId}`);
-        expect(screen.getByText(`/routes/${pacemakerId}`)).toBeTruthy();
-      });
-
-      it('should have an clickable Database icon which links to the belonging Database overview',
-      () => {
-        //need an example id to check if the link is correct
-        const databaseID="HERE_IS_THE_TEST_ID"; // where should i grab the data.
-
-        const [statefulHomeHealthSummary, store] = withState(
-          <HealthSummary/>);
-          
-        //store.dispatch((healthSummaryTableConfig()));
-        //store.dispatch((GlobalHealth()));
-        //store.dispatch(HealthSummary());
-       
-        //store.dispatch(healthSummaryTableConfig());
-
-        store.dispatch(healthSummaryTableConfig());
-        render(statefulHomeHealthSummary);
-
-        //console.log(render(statefulHomeHealthSummary));
-
-        expect(screen.getByText(`/databases/${databaseID}`)).toBeTruthy();
-      });
-
-      it('should have an clickable Sap Instance icon which links to the belonging SAP SYSTEMS overview',
-      () => {
-        //need an example id to check if the link is correct
-        const sapInstanceId = "HERE_IS_THE_TEST_ID"; // where should i grab the data.
-
-        const [statefulHomeHealthSummary, store] = withState(
-          <HealthSummary/>);
-          
-        //store.dispatch((healthSummaryTableConfig()));
-        //store.dispatch((GlobalHealth()));
-        //store.dispatch(HealthSummary());
-       
-        //store.dispatch(healthSummaryTableConfig());
-
-        store.dispatch(healthSummaryTableConfig());
-        render(statefulHomeHealthSummary);
-
-        //console.log(render(statefulHomeHealthSummary));
-        expect(screen.getByText(`/sap_systems/${sapInstanceId}`)).toBeTruthy();
+        expect(screen.getByText(`/routes/blabla`)).toBeTruthy();
       });
 
 
