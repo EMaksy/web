54e26de38fa782831301f15ef4f713f73ae128c5
EMaksy <eugen.maksymenko@suse.com>
2022-10-20 17:43:32 +0200

    Make the mocking work

--- assets/js/components/HealthSummary/HealthSummary.test.jsx
+++ assets/js/components/HealthSummary/HealthSummary.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import 'intersection-observer';
 import '@testing-library/jest-dom';
 import { withState } from '@lib/test-utils';
@@ -9,14 +8,15 @@ import {BrowserRouter, MemoryRouter} from 'react-router-dom';
 
 import { HomeHealthSummary } from './HomeHealthSummary';
 import { setHealthSummary, stopHealthSummaryLoading } from '@state/healthSummary';
-import App from '@testing-library/trento.jsx';
+//import App from '@testing-library/trento.jsx';
 
 describe('HomeHealthSummary icons',() => {
     it('should have an clickable Pacemaker Cluster icon which links to the belonging cluster overview',
       () => {
         const [statefulHomeHealthSummary, store] = withState(<HomeHealthSummary/>);
         //const user = userEvent.setup()
-        console.log('Current directory: ' + process.cwd());
+        //console.log('Current directory: ' + process.cwd());
+        console.log(statefulHomeHealthSummary);
         store.dispatch(setHealthSummary([{
           id: 'string',
           sid: 'string',
@@ -29,16 +29,27 @@ describe('HomeHealthSummary icons',() => {
           sapsystemHealth: "string",
           sid: "string"
         }]));
-      
+
         store.dispatch(stopHealthSummaryLoading());
-
-        render(<App />, {wrapper: BrowserRouter});
-        render(statefulHomeHealthSummary);
-
-        console.log(screen);
+        //console.log(screen.getAllByText('/databases/string'));
+        //render(<App />, {wrapper: BrowserRouter});
+        render(statefulHomeHealthSummary,{wrapper: BrowserRouter});
+        console.log("This is the whole screen");
+        //console.log(screen);
+        console.log(document);
+        //console.log(screen.getByText(`/clusters/string`));
+        //console.log(screen);
         //expect(screen.getByRole('href')).toHaveTextContent(`/routes/${pacemakerId}`);
         //console.log(statefulHomeHealthSummary)
-        expect(screen.getByText(`/routes/string`)).toBeTruthy();
+        console.log(/cluster/id);
+        console.log(/databases/id);
+        console.log((/sap_systems/id));
+
+        const asserted_string  = /cluster/id;
+        expect(screen.getByText(asserted_string)).toBeTruthy();
+        //expect(screen.getByText('a href="/clusters/string">')).toBeTruthy();
+        //expect(screen.getByRole('a', href: '/clusters/string'))
+       // expect(screen.toHaveAttribute('href', '/clusters/string').toBeTruthy());
       });
 
 
